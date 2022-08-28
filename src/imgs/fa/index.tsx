6447d0a4d77cc9436d3ace18e0032cebc3fa2ddd
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2022-08-28 10:17:17 -0500

    add imgs

---
+++ src/imgs/fa/index.tsx
@@ -0,0 +1,21 @@
+import faAddressBook from "./addressBook.png";
+import faBriefcase from "./briefcase.png";
+import faChessPawn from "./chessPawn.png";
+import faCode from "./code.png";
+import faExternalLink from "./externalLink.png";
+import faGraduationCap from "./graduationCap.png";
+import faLanguage from "./language.png";
+import faStar from "./star.png";
+import faUser from "./user.png";
+
+export {
+  faAddressBook,
+  faBriefcase,
+  faChessPawn,
+  faCode,
+  faExternalLink,
+  faGraduationCap,
+  faLanguage,
+  faStar,
+  faUser,
+};
