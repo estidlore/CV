460f32a579e0b2cadd1c900e126f0937a3b46424
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2022-08-28 10:24:46 -0500

    render cv

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import type { FC } from "react";
 import React from "react";
 
+import {
+  basicInfo,
+  complementaryEducation,
+  contact,
+  education,
+  experience,
+  hobbies,
+  languages,
+  profile,
+  skills,
+} from "data/cvInfo";
+import { CV } from "views/CV";
+
 const App: FC = (): JSX.Element => (
-  <div>{"CV content"}</div>
+  <div className={"bg-dark1 flex jc-center"}>
+    <CV
+      basicInfo={basicInfo}
+      complementaryEducation={complementaryEducation}
+      contact={contact}
+      education={education}
+      experience={experience}
+      hobbies={hobbies}
+      languages={languages}
+      profile={profile}
+      skills={skills}
+    />
+  </div>
 );
 
 export { App };
