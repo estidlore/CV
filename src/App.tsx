2b427fad3e36d8f1f9e7411290930501430a7e35
estidlore <estidlore@outlook.com>
2022-12-13 00:12:33 -0500

    merge cv view and app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import type { FC } from "react";
-import React from "react";
+import React, { useCallback } from "react";
 
-import { CV } from "views/CV";
+import { generatePdf, getOptions } from "utils/pdf";
+import { MainPanel } from "views/MainPanel";
+import { SidePanel } from "views/SidePanel";
 
-const App: FC = (): JSX.Element => (
-  <div className={"bg-dark1 ff-arial flex jc-center"}>
-    <CV />
-  </div>
-);
+const options = getOptions("CV", 1120, 1500);
+
+const App: FC = (): JSX.Element => {
+  const handleSave = useCallback(() => {
+    generatePdf(options);
+  }, []);
+
+  return (
+    <div className={"ai-center bg-dark1 ff-arial d-f flex-col"}>
+      <section
+        className={"f2 d-f light2 pos-rel"}
+        id={options.name}
+        style={options.style}
+      >
+        <SidePanel />
+        <MainPanel />
+        <p className={"ai-center b0 d-f light6 r0 m5 pos-abs"}>
+          {"I built this CV with React and jsPDF"}
+        </p>
+      </section>
+      <button
+        className={"bg-primary5 light2 mt3 px3 py2 rnd"}
+        onClick={handleSave}
+      >
+        {"Save"}
+      </button>
+    </div>
+  );
+};
 
 export { App };
